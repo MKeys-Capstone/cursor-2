45dd0771aa2b279dae6ef5732a62912eab3f3f48
MKeys-Capstone <mkeys@capstone-is.com>
2025-02-21 16:26:29 -0500

    scripts

---
+++ update-s3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Define paths
+BUILD_DIR="./dist"
+REACT_APP_BUCKET="matt-test--practice-react-app"
+CLOUDFRONT_DISTRIBUTION_ID="EAD6PU8HYULTL"
+
+# Build the React app
+echo "Building React app..."
+npm install
+npm run build
+
+# Ensure the build folder exists
+if [ ! -d "$BUILD_DIR" ]; then
+  echo "Build folder does not exist. Please run the build command."
+  exit 1
+fi
+echo "React app built successfully."
+
+#Sync S3 Bucket with built React app
+echo "Syncing S3 Bucket with built React app..."
+aws s3 sync $BUILD_DIR s3://${REACT_APP_BUCKET}
+
+# Invalidate CloudFront Distribution
+echo "Invalidating CloudFront Distribution..."
+aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*"
+echo "CloudFront Distribution invalidated successfully."
